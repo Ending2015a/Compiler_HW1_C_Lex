4a235ea9e6d9805e4d7936f4e75f7aaeb54313d2
103062372 <zexlus1126@gapp.nthu.edu.tw>
2017-04-10 12:57:23 +0800

    Fix some bugs

---
+++ my_testcase.c
@@ -0,0 +1,72 @@
+#pragma source on
+#pragma source off
+#pragma source on
+
+int main(void){
+	int a = 123123;
+	int b =2432;
+	char *x = "Hellooooo\"\"\"\"\"\""
+#pragma token off
+	a = a+b;
+	b = a+c;
+	/*
+#pragma token on
+*/
+int a = 123123212;
+#pragma token on
+	int a = 1/*/*/*/*/*/2;
+	/*
+	* / ABC
+	 / * BCD
+	/ * / CDE
+	 * / * DEFG
+	* / * / ASSDSF
+	 / * / * FDFSDFDF/*sdfds#pragma
+	/ * / * / 
+	 * / * /
+	* / * /
+	 / * /
+	/ * /
+	 * /
+	* /
+	*/
+
+	double abd = 3e-10;
+	abd = 3e-10+2*2.1+3e+12-10+10-1e-9*10*12+0+9+1-4e-4;
+	char *gg = "sdfdsf d d f\" \' \' ";; // /**/
+	#pragma source on /**/
+
+	/*this part will show the advance abilities of my scanner*/
+
+
+
+	/*
+		123212321232123
+	*/ #pragma /*2343234*/ token      /************/ off  //sdklfjkljrjhfahlgjkaer
+
+	int h = 1;
+/*
+sdfdsdfdsdfdsdfdsdfdsdfdsdfdsdfdsdfdsdfdsdfdsdfdsdfd++--++--****/  /**/ /**/  #pragma   /**/ /**/ token on
+
+	char *m = "This is\
+my String!!!!!!\"\"\"";
+
+
+	int a = 0xfff;
+
+
+	float b = 0.0001f;
+
+	
+	double asd = 123456L;
+
+	unsigned long long qq11 = 12312165498789LL;
+
+
+	return 0;
+}
+
+
+//stop
+#pragma source off int
+
